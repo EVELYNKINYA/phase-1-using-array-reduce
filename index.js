70d0f13b773efddb8b2283e4d81ea4251e0d1c6b
EVELYNKINYA <evelynkinya156@gmail.com>
2023-12-21 00:00:55 +0300

    done with assignment

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
-
-// Code your solution here
+function reducer(batteryCounts) {
+    // Assuming batteryCounts is an array of numbers representing the counts of batteries
+    return batteryCounts.reduce((sum, count) => sum + count, 0);
+  }
+  
+  
+  const batteryCounts = [5, 8, 3, 15]; 
+  const totalBatteries = reducer(batteryCounts);
+  console.log(totalBatteries); // Output: 31
+  
